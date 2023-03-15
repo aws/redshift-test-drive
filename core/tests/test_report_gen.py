568c1e2770a561718a4551fb37d6bd228715f2ef
Jessie Chen <jiezhec@amazon.com>
2023-03-15 14:50:47 -0700

    fix test_read_data_breakdown

--- core/tests/test_report_gen.py
+++ core/tests/test_report_gen.py
@@ -321,9 +321,9 @@ class TestReportGen(unittest.TestCase):
 
 
     @patch('pandas.DataFrame', return_value=df_mock)
+    @patch('common.aws_service.s3_resource_put_object', side_effect=Exception("Boom"))
     @patch('pandas.DataFrame.round', return_value=df_mock)
-    @patch('common.aws_service.s3_resource_put_object')
-    def test_read_data_breakdown(self, mock_s3_put_obj, mock_round, mock_df):
+    def test_read_data_fail(self, mock_round, mock_s3_put_obj, mock_df):
         mock_tables = {"table_name": {"type": "breakdown"}}
         mock_report = Mock()
         mock_report.tables.return_value = mock_tables
