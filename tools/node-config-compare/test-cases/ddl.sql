5f1ddb7cc998fde4226892cb9e4843d41cddbe8b
jiezhen-chen <121250701+jiezhen-chen@users.noreply.github.com>
2023-03-16 14:41:24 -0700

    Delete tools/node-config-compare directory

--- tools/node-config-compare/test-cases/ddl.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists example_table
-(id INTEGER IDENTITY(1, 1) NOT NULL, column_value varchar(10), insert_timestamp timestamp default sysdate);
-
-insert into example_table (column_value) values('data');
