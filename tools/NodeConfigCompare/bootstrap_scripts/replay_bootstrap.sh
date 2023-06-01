ee00f5bbcbb89a806723e79b43033255a32302f7
karle-nishi <123670211+karle-nishi@users.noreply.github.com>
2023-06-01 15:40:52 -0700

    Update replay_bootstrap.sh

--- tools/NodeConfigCompare/bootstrap_scripts/replay_bootstrap.sh
+++ tools/NodeConfigCompare/bootstrap_scripts/replay_bootstrap.sh
@@ -40,13 +40,13 @@ sed -i "s#workload_location: \".*\"#workload_location: \"$WORKLOAD_LOCATION\"#g"
 sed -i "s#target_cluster_endpoint: \".*\"#target_cluster_endpoint: \"$CLUSTER_ENDPOINT\"#g" config/replay.yaml
 sed -i "s#target_cluster_region: \".*\"#target_cluster_region: \"$TARGET_CLUSTER_REGION\"#g" config/replay.yaml
 
-if [ "$SIMPLE_REPLAY_UNLOAD_STATEMENTS" == "true" ]; then
+if [[ "$SIMPLE_REPLAY_UNLOAD_STATEMENTS" == "true" ]]; then
     sed -i "s#unload_iam_role: \".*\"#unload_iam_role: \"$REDSHIFT_IAM_ROLE\"#g" config/replay.yaml
     sed -i "s#replay_output: \".*\"#replay_output: \"s3://$BUCKET_NAME/$REPLAY_PREFIX/$WHAT_IF_TIMESTAMP/$CLUSTER_IDENTIFIER\"#g" config/replay.yaml
 fi
 
 
-if [ "$SIMPLE_REPLAY_ANALYSIS_EXECUTION" == "true" ]; then
+if [["$SIMPLE_REPLAY_ANALYSIS_EXECUTION" == "true"]]; then
     sed -i "s#analysis_iam_role: \".*\"#analysis_iam_role: \"$REDSHIFT_IAM_ROLE\"#g" config/replay.yaml
     sed -i "s#analysis_output: \".*\"#analysis_output: \"$WORKLOAD_LOCATION\"#g" config/replay.yaml
 fi
